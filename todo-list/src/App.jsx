b88691814428e5f234e887f5ff5829bfae09bf8b
somya-ctrl <agarwalsomya224@gmail.com>
2026-03-05 21:29:21 +0530

    initial commit

---
+++ todo-list/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <h1 className='text-3xl bg-green-500 text-white font-bold p-4'>
+      hello
+    </h1>
+  )
+}
+
+export default App
